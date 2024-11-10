2fd113693687a955fd3e9b596389f07635fa8141
Alex Bussey <alex@alexbussey.com>
2024-11-10 16:59:12 -0500

    Create post button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { UserNav } from "@/components/user-nav";
 import { Toaster } from "@/components/ui/sonner";
+import Link from "next/link";
+import { CreatePost } from "@/components/create-post";
+import { CreatePostButton } from "@/components/create-post-button";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -31,35 +34,23 @@ export default function RootLayout({
         >
           <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
             <header className="flex items-center w-full justify-between py-10">
-              <a className="break-words" href="/">
+              <Link className="break-words" href="/">
                 Friends of Amy Breen
-              </a>
+              </Link>
               <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
                 <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
-                  <a className="block" href="/">
+                  <Link className="block" href="/">
                     Updates
-                  </a>
+                  </Link>
+                  {}
                   {/* <a className="block" href="/">
                     Memories
                   </a> */}
+                  <CreatePostButton />
                 </div>
                 <UserNav />
                 <div className="mr-5 flex items-center">
                   <ThemeSwitcher />
-                </div>
-                <div className="sm:hidden">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="h-8 w-8 text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
                 </div>
               </div>
             </header>
